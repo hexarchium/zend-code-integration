73cdcb0d03b9aa516513b23b9b60fa5a1cc226b5
TimiTao <kunicki.tomasz@gmail.com>
2017-01-15 23:34:23 +0100

    remove repository

--- src/Projector/ClassStructureProjector.php
+++ src/Projector/ClassStructureProjector.php
@@ -9,6 +9,15 @@ class ClassStructureProjector
 {
     public function onClassCreated(ClassStructureAdded $classStructureAdded)
     {
+        $name = $classStructureAdded->getPayload()['name'];
+        $namespaceName = $classStructureAdded->getPayload()['namespace'];
+
+
         $class = new ClassGenerator();
+        $class->setName($name)
+            ->setNamespaceName($namespaceName);
+
+        $class->generate();
+
     }
 }
